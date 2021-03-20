566bd5a6da0303f8ff0fb5e4a8056d18115cac6e
Laura Messa Travin <laura.travin@gmail.com>
2021-03-20 04:45:16 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,7 +12,7 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
 
     @post.update(post_params)
-    if @post.valid
+    if @post.valid?
        redirect_to post_path(@post)
     else
       render edit:
